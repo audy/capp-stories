4f8f052495d9f7a9c47e12c2cd358bdba2f7ee67
Austin Davis-Richardson <harekrishna@gmail.com>
2017-07-18 16:56:14 -0700

    8

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 var tracery = require('tracery-grammar')
-var grammar = require('grammar')
+var fs = require('fs')
 
-var grammar = tracery.createGrammar()
+var grammar = JSON.parse(fs.readFileSync('grammar.json', 'utf8'))
 
-console.log(grammar.flatten('#origin#'))
+var stories = tracery.createGrammar(grammar)
+
+console.log(stories.flatten('#origin#'))
